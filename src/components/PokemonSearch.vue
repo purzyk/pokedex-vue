bc22a86c96570b7ce2b4a6e0374495c91ed86823
Piotr Purzycki <piotr@purzycki.pl>
2022-12-14 08:36:39 +0100

    autocomplete functionality

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -1,14 +1,14 @@
 <template>
 
-    <div class="searchbar">
+    <div class="searchbar" v-if="pokemons">
         <form @submit.prevent="setPokemonUrl">
             <div class="container">
                 <input type="text" placeholder="Type your query" v-model="search" @blur="toggle = false"
                     @focus="toggle = true">
                 <div class="results" v-if="toggle">
-                    <div class="result" v-for="book in newBooks" :key="book">
-                        <span @click="selectResult(book)" @mousedown.prevent>
-                            {{ book.name }}
+                    <div class="result" v-for="pokemon in pokemonsAutoComplete" :key="pokemon">
+                        <span @click="selectResult(pokemon)" @mousedown.prevent>
+                            {{ pokemon.name }}
                         </span>
                     </div>
                 </div>
@@ -28,10 +28,10 @@ export default {
     watch: {
         search(val) {
             if (val.length > 2) {
-                this.newBooks = [];
-                this.books.forEach(element => {
+                this.pokemonsAutoComplete = [];
+                this.pokemons.forEach(element => {
                     if (element.name.includes(val) && val != "") {
-                        this.newBooks.push(element);
+                        this.pokemonsAutoComplete.push(element);
                     }
                 });
             }
@@ -41,55 +41,30 @@ export default {
         return {
             search: '',
             toggle: false,
-            books: [
-                { name: 'bulbasaur' },
-                { name: 'venusaur' },
-            ],
-            newBooks: [],
+            pokemons: [],
+            pokemonsAutoComplete: [],
             searchvalue: '',
         }
     },
     methods: {
-        selectResult(book) {
-            this.search = book.name;
+        selectResult(pokemon) {
+            this.search = pokemon.name;
+        },
+        loadPosts() {
+            fetch(this.apiUrl + '?limit=100000&offset=0')
+                .then(res => res.json())
+                .then((data) => {
+                    this.pokemons = data.results;
+                })
+                .catch(err => console.log(err.message))
         },
         setPokemonUrl(url) {
             if (this.search !== '')
                 this.$emit('setPokemonUrl', this.apiUrl + this.search);
         }
-    }
+    },
+    mounted() {
+        this.loadPosts();
+    },
 }
 </script>
-
-<style scoped>
-.searchbar {
-    position: relative;
-    width: 100%;
-    max-width: 510px;
-    padding-bottom: 20px;
-}
-
-input {
-    border: none;
-    outline: none;
-    border-radius: 5px;
-    padding: 10px 40px 10px 10px;
-    width: calc(100% - 50px);
-    font-size: 1rem;
-    box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
-        0 10px 10px rgba(0, 0, 0, .2);
-}
-
-.legenda {
-    color: #fff;
-}
-
-i {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 1.25rem;
-    color: #0A2E50;
-    cursor: pointer;
-}
-</style>
